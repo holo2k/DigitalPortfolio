d69f605675b166bc48778f1a723317c2f8ba3792
Никита <Nikita.Belikov@urfu.me>
2024-02-19 01:00:35 +0500

    0.0

---
+++ DigitalPortfolio.Domain/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalPortfolio.Domain.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        internal string Name { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+
+        private bool Admin { get; }
+        public User(int id, string name, string email, string pass) 
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = pass;
+        }
+    }
+}
---
+++ DigitalPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DigitalPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
